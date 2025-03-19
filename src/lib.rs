8175c8edcdbea825d0ebd04105ec646e2b51ba59
Ben Strobel <benedikt.strobel@tum.de>
2025-03-19 14:25:45 +0100

    Remove contract build dependency

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,34 @@
+use std::sync::{Arc, RwLock};
+
+use serde::{Deserialize, Serialize};
+
 wit_bindgen::generate!({
-    path: "wit/media-provider.wit",
+    path: "wit/media-provider/media-provider.wit",
     world: "media-provider-plugin-world"
 });
+
+#[derive(Serialize, Deserialize)]
+struct Config {
+    file_extensions: Vec<String>,
+}
+
+lazy_static::lazy_static! {
+    static ref CONFIG: Arc<RwLock<Option<Config>>> = Arc::new(RwLock::new(None));
+}
 
 struct FilesystemMediaProvider;
 
 impl Guest for FilesystemMediaProvider {
-    fn index_path(
-        path: _rt::String,
-        job_id: _rt::String,
-    ) -> Result<u32, Error> {
-        emit_indexed_folder_event(&Folder {name: "".to_string(), relative_path: "".to_string()});
-        emit_indexed_file_event(&File { name: "".to_string(), relative_path: "".to_string(), duration: None });
+    fn index_path(path: _rt::String, job_id: _rt::String) -> Result<u32, Error> {
+        emit_indexed_folder_event(&Folder {
+            name: "".to_string(),
+            relative_path: "".to_string(),
+        });
+        emit_indexed_file_event(&File {
+            name: "".to_string(),
+            relative_path: "".to_string(),
+            duration: None,
+        });
         return Result::Err(Error::NotImplemented);
     }
 }
