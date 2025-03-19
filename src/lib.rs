7b253859c02e417a6de59b1c68a8807efce1b3ab
Ben Strobel <benedikt.strobel@tum.de>
2025-03-19 17:17:29 +0100

    Refactor logic using glob library

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use std::{
-    path::PathBuf,
+    path::{Path, PathBuf},
     str::FromStr,
     sync::{Arc, RwLock},
 };
 
+use glob::glob;
 use serde::{Deserialize, Serialize};
 use spaeher::media_provider_plugin::logging::log_warn;
 
@@ -25,87 +26,65 @@ lazy_static::lazy_static! {
 
 struct FilesystemMediaProvider;
 
-impl FilesystemMediaProvider {
-    fn index_path(base_path: String, path: String, action_id: String) -> Result<u32, Error> {
+impl Guest for FilesystemMediaProvider {
+    fn index_path(base_path_str: String, action_id: String) -> Result<u32, Error> {
         let config = CONFIG
             .read()
             .map_err(|_| Error::ConfigInvalid("Failed to acquire read lock".to_string()))?
             .clone()
             .ok_or(Error::ConfigInvalid("Config not initialized".to_string()))?;
-        let path = PathBuf::from_str(&path).map_err(|e| Error::PathInvalid(e.to_string()))?;
+        let base_path =
+            PathBuf::from_str(&base_path_str).map_err(|e| Error::PathInvalid(e.to_string()))?;
         let mut total_indexed_files = 0u32;
-        for entry in
-            std::fs::read_dir(path.clone()).map_err(|e| Error::PathInvalid(e.to_string()))?
-        {
+        let glob_pattern = format!(
+            "{}/**/*.{{{}}}",
+            &base_path_str,
+            config
+                .file_extensions
+                .iter()
+                .map(|e| format!(".{}", e))
+                .collect::<Vec<String>>()
+                .join(",")
+        );
+        for entry in glob(&glob_pattern).map_err(|e| Error::PathInvalid(e.to_string()))? {
             let Ok(entry) = entry else {
                 continue;
             };
-            let entry_path = entry.path();
-            if entry_path.is_dir() {
-                let Some(entry_path_str) = entry_path.to_str() else {
-                    log_warn(&format!(
-                        "Failed to convert path to string: {:?}",
-                        entry_path
-                    ));
-                    continue;
-                };
-                match Self::index_path(
-                    base_path.clone(),
-                    entry_path_str.to_owned(),
-                    action_id.clone(),
-                ) {
-                    Ok(indexed_files) => {
-                        total_indexed_files += indexed_files;
-                    }
-                    Err(err) => {
-                        log_warn(&format!(
-                            "Failed to index subdirectory: '{}': {}",
-                            entry_path_str, err
-                        ));
-                        continue;
-                    }
-                }
-            } else {
-                let file_name = entry.file_name();
-                let Some(name_str) = file_name.to_str() else {
-                    log_warn(&format!(
-                        "Failed to convert file name to string: {:?}",
-                        file_name
-                    ));
-                    continue;
-                };
-                for ext in &config.file_extensions {
-                    if name_str.ends_with(ext) {
-                        let path = path.clone();
-                        let Ok(Some(relative_path)) =
-                            path.strip_prefix(base_path.clone()).map(|p| p.to_str())
-                        else {
-                            log_warn(&format!(
-                                "Failed to compute relative path for path: {:?}",
-                                path
-                            ));
-                            continue;
-                        };
-                        emit_indexed_file_event(
-                            &File {
-                                name: name_str.to_string(),
-                                relative_path: relative_path.to_string(),
-                                duration: None,
-                            },
-                            &action_id,
-                        );
-                        break;
-                    }
-                }
+            if entry.is_dir() {
+                continue;
             }
+            let Some(Some(file_name)) = entry.file_name().map(|f| f.to_str()) else {
+                log_warn(&format!(
+                    "Failed to convert file name to string: {:?}",
+                    entry.file_name()
+                ));
+                continue;
+            };
+            let Ok(relative_path) = Path::strip_prefix(&entry, base_path.clone()) else {
+                log_warn(&format!(
+                    "Failed to compute relative path for path: {:?}",
+                    entry
+                ));
+                continue;
+            };
+            let Some(relative_path_str) = relative_path.to_str() else {
+                log_warn(&format!(
+                    "Failed to convert relative path to string: {:?}",
+                    relative_path
+                ));
+                continue;
+            };
+            emit_indexed_file_event(
+                &File {
+                    name: file_name.to_string(),
+                    relative_path: relative_path_str.to_string(),
+                    duration: None,
+                },
+                &action_id,
+            );
+            total_indexed_files += 1;
         }
         return Result::Ok(total_indexed_files);
-    }
-}
-
-impl Guest for FilesystemMediaProvider {
-    fn index_path(path: String, action_id: String) -> Result<u32, Error> {
-        Self::index_path(path.clone(), path, action_id)
     }
 
     fn on_config_changed(_action_id: String) -> Result<(), Error> {
